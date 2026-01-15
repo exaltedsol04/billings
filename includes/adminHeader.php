9f48a9f924920b342f3b03019a5935039cbd1ae2
Exalted 06 <exaltedsol06@gmail.com>
2026-01-15 16:20:23 +0530

    add

--- includes/adminHeader.php
+++ includes/adminHeader.php
@@ -52,18 +52,19 @@
         <a href="javascript:;"><i class="material-icons-outlined">menu</i></a>
       </div>
       <div class="search-bar flex-grow-1">
+		<h5 class="user-name mb-0 fw-bold">Hello, <?php echo $_SESSION['ROLE_ID']==1 ? ''.$_SESSION['USERNAME'] : ''.$_SESSION['USERNAME']; ?></h5>
       </div>
       <ul class="navbar-nav gap-1 nav-right-links align-items-center">
         <li class="nav-item dropdown">
           <a href="javascrpt:;" class="dropdown-toggle dropdown-toggle-nocaret" data-bs-toggle="dropdown">
-             <img src="assets/images/avatars/01.png" class="rounded-circle p-1 border" width="45" height="45" alt="">
+             <img src="assets/images/01.png" class="rounded-circle p-1 border" width="45" height="45" alt="">
           </a>
           <div class="dropdown-menu dropdown-user dropdown-menu-end shadow">
             <a class="dropdown-item  gap-2 py-2" href="javascript:;">
               <div class="text-center">
-                <img src="assets/images/avatars/01.png" class="rounded-circle p-1 shadow mb-3" width="90" height="90"
+                <img src="assets/images/01.png" class="rounded-circle p-1 shadow mb-3" width="90" height="90"
                   alt="">
-                <h5 class="user-name mb-0 fw-bold">Hello, <?php echo $_SESSION['ROLE_ID']==1 ? ''.$_SESSION['USERNAME'] : ''.$_SESSION['USERNAME']; ?></h5>
+                <h5 class="user-name mb-0 fw-bold">Hello, <?php echo $_SESSION['ROLE_ID']==1 ? 'Administrator' : 'Seller'; ?></h5>
               </div>
             </a>
             <hr class="dropdown-divider">
