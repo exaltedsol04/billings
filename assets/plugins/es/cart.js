ff4ac55bc36b2a38399441cf7a8e1deab078a490
exaltedsol04 <exaltedsol04@gmail.com>
2026-01-16 20:17:58 +0530

    comment

--- assets/plugins/es/cart.js
+++ assets/plugins/es/cart.js
@@ -8,6 +8,12 @@ let totalAmountShow = document.getElementById("total_amount_show");
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
+$(document).on("keydown", ".qty-input", function (e) {
+  if (e.key === "-" || e.key === "e") {
+    e.preventDefault();
+  }
+});
+
 /**
  * ! used to add to cart
  */
@@ -120,7 +126,7 @@ let generateCartItems = () => {
 						<span class="input-group-btn">
 							<button type="button" class="btn btn-default btn-qty" style="cursor:pointer" onclick="decrement(${id})">−</button>
 						</span>
-						<input type="text" class="form-control text-center qty-input" value="${qty}" min="1" id="qty_${id}" name="qty[]">
+						<input type="number" class="form-control text-center qty-input" value="${qty}" min="1" oninput="updateQty(${id}, this.value)" id="qty_${id}" name="qty[]">
 						<span class="input-group-btn">
 							<button type="button" class="btn btn-default btn-qty" style="cursor:pointer" onclick="increment(${id})" >+</button>
 						</span>
@@ -206,6 +212,35 @@ let decrement = (id) => {
   //generateCartItems();
   localStorage.setItem("data", JSON.stringify(basket));
   $('#check-stock-pay-div').html(' ');
+  setTimeout(function () {
+	generateCartItems();
+  }, 500);
+};
+
+/**
+ * ! used to update the selected product item quantity by value
+ */
+
+let updateQty = (id, value) => {
+  let qty = parseInt(value);
+
+  // Prevent empty, zero, or negative values
+  if (isNaN(qty) || qty < 1) {
+    qty = 1;
+    document.getElementById(`qty_${id}`).value = 1;
+  }
+
+  let search = basket.find((x) => x.id === id);
+  if (!search) return;
+
+  search.qty = qty;
+  search.item = qty; // keep item count in sync
+
+  localStorage.setItem("data", JSON.stringify(basket));
+
+  calculation();
+  TotalAmount();
+  
   setTimeout(function () {
 	generateCartItems();
   }, 500);
