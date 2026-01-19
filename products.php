2be5fa9ccfbabf7ed6f9d5f4e3e97b47113feca3
exaltedsol04 <exaltedsol04@gmail.com>
2026-01-19 19:55:05 +0530

    comment

--- products.php
+++ products.php
@@ -11,6 +11,26 @@
 	{
 		$seller_id = $_GET['sid'];
 		$orWhere = "AND pr.seller_id ='" .$seller_id. "'";
+		
+		$fields = "pr.id, pr.product_id, pr.status, SUM(pr.stock) as total_stock, u.name as stock_unit_name, pv.measurement, p.name, p.barcode";
+		$tables = PRODUCT_STOCK_TRANSACTION . " pr
+		INNER JOIN " . PRODUCT_VARIANTS . " pv ON pr.product_variant_id = pv.id
+		INNER JOIN " . PRODUCTS . " p ON p.id = pr.product_id
+		INNER JOIN " . UNITS . " u ON u.id = pv.stock_unit_id";
+		$where = "WHERE pr.status=1 ".$orWhere." GROUP BY pr.product_variant_id HAVING SUM(pr.stock) > 0";
+		$params = [];
+		$sqlQueryP = $general_cls_call->select_join_query($fields, $tables, $where, $params, 2);
+	}
+	else{
+		
+		$fields = "pv.id, pv.product_id, pv.type, pv.stock, pv.measurement, pv.discounted_price, p.name, p.image, p.barcode, u.name as unit_name";
+		$tables = PRODUCT_VARIANTS . " pv
+		INNER JOIN " . PRODUCTS . " p ON p.id = pv.product_id 
+		INNER JOIN " . UNITS . " u ON u.id = pv.stock_unit_id";
+		$where = "WHERE 1 GROUP BY pv.id ORDER BY p.name";
+		$params = [];
+		$sqlQueryP = $general_cls_call->select_join_query($fields, $tables, $where, $params, 2);
+		//echo "<pre>";print_r($sqlQueryP);die;
 	}
 ?>
 	<!-- ######### HEADER START ############### -->
@@ -67,15 +87,16 @@
 							<table id="example2" class="table table-striped table-bordered">
 								<thead>
 									<tr>
-										<!--<td></td>-->
-										<td><input type="text" class="form-control" id="search-zero" placeholder="Search by barcode"></td>
-										<td><input type="text" class="form-control" id="search-one" placeholder="Search by product name"></td>
+										<td></td>
+										<td><input type="text" class="form-control" id="search-one" placeholder="Search by barcode"></td>
+										<td><input type="text" class="form-control" id="search-two" placeholder="Search by product name"></td>
 										<td></td>
 										<td></td>
 										
 									</tr>
 								  <tr class="text-center">
 									<!--<th>Image</th>-->
+									<th>Sl. No.</th>
 									<th>Barcode</th>
 									<th>Name</th>
 									<th>Available Stock</th>
@@ -85,23 +106,24 @@
 								<tbody>
 									<?php 
 								
-									$fields = "pr.id, pr.product_id, pr.status, SUM(pr.stock) as total_stock, u.name as stock_unit_name, pv.measurement, p.name, p.barcode";
+									/*$fields = "pr.id, pr.product_id, pr.status, SUM(pr.stock) as total_stock, u.name as stock_unit_name, pv.measurement, p.name, p.barcode";
 						$tables = PRODUCT_STOCK_TRANSACTION . " pr
 						INNER JOIN " . PRODUCT_VARIANTS . " pv ON pr.product_variant_id = pv.id
 						INNER JOIN " . PRODUCTS . " p ON p.id = pr.product_id
 						INNER JOIN " . UNITS . " u ON u.id = pv.stock_unit_id";
 						$where = "WHERE pr.status=1 ".$orWhere." GROUP BY pr.product_variant_id HAVING SUM(pr.stock) > 0";
 						$params = [];
-						$sqlQuery = $general_cls_call->select_join_query($fields, $tables, $where, $params, 2);
+						$sqlQueryP = $general_cls_call->select_join_query($fields, $tables, $where, $params, 2);*/
 									//echo "<pre>";print_r($sqlQuery);die;
-									if($sqlQuery[0] != '')
+									if($sqlQueryP[0] != '')
 									{
 										$i = 1;
-										foreach($sqlQuery as $arr)
+										foreach($sqlQueryP as $k=>$arr)
 										{	
 									?>
 											  <tr class="text-center" id="dataRow<?php echo($arr->id);?>">
-												<td><?PHP echo $arr->barcode; ?></td>
+												<td><?PHP echo $k+1; ?></td>
+												<td><?PHP echo !empty($arr->barcode) ? $arr->barcode : 'N/A'; ?></td>
 												<td><?PHP echo $general_cls_call->cart_product_name($arr->name); ?></td>
 												<td><?PHP echo $arr->total_stock ?></td>
 												<td><?PHP echo $arr->measurement. ' ' .$arr->stock_unit_name; ?></td>
@@ -114,7 +136,7 @@
 									{
 								?>
 											  <tr>
-												<td colspan="4">
+												<td colspan="5">
 												No record found.
 												</td>
 											  </tr>
