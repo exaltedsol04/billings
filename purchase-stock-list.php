792638a906b095e90258af2967de31312d6ef795
exaltedsol04 <exaltedsol04@gmail.com>
2026-01-21 14:50:41 +0530

    comment

--- purchase-stock-list.php
+++ purchase-stock-list.php
@@ -47,10 +47,10 @@
 										<td></td>
 									</tr>
 								  <tr class="text-center">
-									<th style="width:100px">Invoice Id</th>
+									<th style="width:100px">Sl. No.</th>
 									<th>Vendors</th>
 									<th>Product Name</th>
-									<th>Product Variant Name</th>
+									<th>Variant Name</th>
 									<th>Purchase Stock</th>
 									<th>Purchase Date</th>
 									<th>Action</th>
@@ -58,38 +58,32 @@
 								</thead>
 								<tbody>
 									<?php
-									if($_SESSION['USER_ID'] == 1)
-									{
-										$where = "WHERE 1";
-										$params = [];
-									}
-									else{
-										$where = "WHERE pos_user_id=:pos_user_id";
-										$params = [
-											':pos_user_id'	=>	$_SESSION['USER_ID']
-										];
-									}
+									$fields = "asp.stock, asp.created_at, pv.measurement, p.name, p.image, p.barcode, u.name as unit_name, v.name as vendor";
 									
-									$sqlQuery = $general_cls_call->select_query("*", POS_ORDERS, $where, $params, 2);
-						
+									$tables = ADMIN_STOCK_PURCHASE_LIST . " asp
+										INNER JOIN " . PRODUCT_VARIANTS . " pv ON pv.id = asp.product_variant_id
+										INNER JOIN " . PRODUCTS . " p ON p.id = asp.product_id
+										INNER JOIN " . UNITS . " u ON u.id = pv.stock_unit_id 
+										INNER JOIN " . VENDORS . " v ON v.id = asp.vendor_id";
+										
+									$where = "WHERE 1";
+									$params = [];
+									
+									$sqlQuery = $general_cls_call->select_join_query($fields, $tables, $where, $params, 2);
+									//echo "<pre>";print_r($sqlQuery);die;
 									if($sqlQuery[0] != '')
 									{
 										$i = 1;
-										foreach($sqlQuery as $selectValue)
+										foreach($sqlQuery as $k=>$selectValue)
 										{
-											$customer = $general_cls_call->select_query("*", SELLERS, "WHERE admin_id=:admin_id", [':admin_id' => $selectValue->pos_user_id], 1);
-
-											/*$pos_order_item = $general_cls_call->select_query("*", POS_ORDERS_ITEMS, "WHERE pos_order_id=:pos_order_id", [':pos_order_id' => $selectValue->id], 1);*/
-
-											$pos_order_item = $general_cls_call->select_query_sum( POS_ORDERS_ITEMS, "WHERE pos_order_id =:pos_order_id", array(':pos_order_id'=> $selectValue->id), 'total_price');											
 									?>
 									  <tr id="dataRow<?php echo($selectValue->id);?>" class="text-center">
-										<td style="width:100px"><?PHP echo $selectValue->id; ?></td>
-										<td><?PHP echo $customer->name; ?></td>
-										<td><?PHP echo $customer->mobile; ?></td>
+									    <td style="width:100px"><?php echo $k+1 ;?></td>
+										<td style="width:100px"><?PHP echo $selectValue->vendor; ?></td>
+										<td><?PHP echo $general_cls_call->cart_product_name($selectValue->name); ?></td>
+										<td><?PHP echo $selectValue->measurement.'  '.$selectValue->unit_name; ?></td>
+										<td><?PHP echo $selectValue->stock; ?></td>
 										<td><?PHP echo $general_cls_call->change_date_format($selectValue->created_at, 'j M Y g:i A'); ?></td>
-										<td>₹<?PHP echo $pos_order_item->total; ?></td>
-										<td></td>
 										<td><a href="<?php echo SITE_URL.'invoices-view'; ?>?order_id=<?php echo($selectValue->id);?>&mode=1"><i class="lni lni-keyword-research"></i></a></td>
 									  </tr>
 										<?PHP
