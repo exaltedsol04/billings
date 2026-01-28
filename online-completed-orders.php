8703b2a5f3384785147686d2f6a0c942733005dc
exaltedsol04 <exaltedsol04@gmail.com>
2026-01-28 14:55:39 +0530

    comment

--- online-completed-orders.php
+++ online-completed-orders.php
@@ -4,7 +4,46 @@
 	$pageAccessRoleIds = [1,3];
 	$general_cls_call->validation_check($_SESSION['USER_ID'], $_SESSION['ROLE_ID'], $pageAccessRoleIds, SITE_URL);// VALIDATION CHEK
 	ob_start();
-
+	
+		if($_SERVER['REQUEST_METHOD'] == "POST" && (isset($_POST['btnUser'])) && $_POST['btnUser'] === "SAVE")
+		{
+			$fromDate = $_POST['fromDate'];
+			$toDate = $_POST['toDate'];
+			$whereDateRange = "AND os.created_at >= :fromDate AND os.created_at < DATE_ADD(:toDate, INTERVAL 1 DAY)";
+			
+			if($_SESSION['ROLE_ID'] == 1)
+			{
+				$params = [
+					':active_status' => 6,
+					':fromDate' => $_POST['fromDate'],
+					':toDate'   => $_POST['toDate']
+				];
+			}
+			else{
+				$params = [
+					':active_status' => 6,
+					':seller_id'=> $_SESSION['SELLER_ID'],
+					':fromDate' => $_POST['fromDate'],
+					':toDate'   => $_POST['toDate']
+				];
+			}
+		}
+		else
+		{
+			$whereDateRange = 'AND DATE(os.created_at) = CURDATE()';
+			if($_SESSION['ROLE_ID'] == 1)
+			{
+				$params = [
+					':active_status' => 6
+				];
+			}
+			else{
+				$params = [
+					':seller_id'=> $_SESSION['SELLER_ID'],
+					':active_status'=> 6
+				];
+			}
+		}
 	ob_end_flush();
 ?>
 	<!-- ######### HEADER START ############### -->
@@ -36,20 +75,20 @@
 						<hr>
 						<div class="card">
 							<div class="card-body">
-								<form class="row g-4" method="post">
+								<form class="row g-4" method="post" action="">
 									<div class="col-md-6">
 										<label for="input1" class="form-label">From date</label>
-										<input type="text" id="fromDate" class="form-control" placeholder="Start Date" readonly>
+										<input type="text" name="fromDate" id="fromDate" class="form-control" placeholder="Start Date" readonly>
 									</div>
 									<div class="col-md-6">
 										<label for="input5" class="form-label">To date</label>
-										<input type="text" id="toDate" class="form-control" placeholder="End Date" readonly>
+										<input type="text" name="toDate" id="toDate" class="form-control" placeholder="End Date" readonly>
 									</div>
 									
 									<div class="col-md-12">
 									  <div class="d-md-flex d-grid justify-content-md-between">
 										<button type="reset" class="btn btn-grd btn-grd-info px-4">Reset</button>
-										<button type="button" class="btn btn-grd btn-grd-danger px-4">Search</button>
+										<button type="submit" class="btn btn-grd btn-grd-danger px-4" name="btnUser" value="SAVE">Search</button>
 									  </div>
 									</div>
 								</form>
@@ -86,26 +125,27 @@
 								<?php
 									if($_SESSION['ROLE_ID'] == 1) {
 										$where = "WHERE oi.active_status=:active_status
+										". $whereDateRange ."
 											  GROUP BY oi.order_id
 											  ORDER BY 
 												  CASE 
 													  WHEN o.order_type = 'slot' THEN o.from_time
 													  ELSE o.created_at
 												  END DESC";
-										$params = [':active_status'=> 6];	
 									} else {
 										$where = "WHERE oi.seller_id=:seller_id 
 											  AND oi.active_status=:active_status
+											  ". $whereDateRange ."
 											  GROUP BY oi.order_id
 											  ORDER BY 
 												  CASE 
 													  WHEN o.order_type = 'slot' THEN o.from_time
 													  ELSE o.created_at
 												  END DESC";
-										$params = [
+										/*$params = [
 											':seller_id'=> $_SESSION['SELLER_ID'],
 											':active_status'=> 6
-										];
+										];*/
 									}
 									
 									$fields = "o.id, o.orders_id, o.final_total, o.user_id, o.delivery_time, o.status, o.packing_charge, o.order_type, o.from_time, o.to_time, o.instant_delivery_time, o.created_at, o.active_status, SUM(oi.sub_total) AS orders_items_sub_total, u.name AS customer_name, osl.status AS orders_status_list_status, os.created_at AS orders_statuses_created_at";
