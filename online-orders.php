eaeac620921dc59b38a56565f0f14ee4863a8784
Exalted 06 <exaltedsol06@gmail.com>
2026-02-05 14:44:01 +0530

    add

--- online-orders.php
+++ online-orders.php
@@ -162,7 +162,9 @@
 										<td class="text-center"><span class="d-none"><?PHP echo $delivery_time; ?></span><?PHP echo $general_cls_call->change_date_format($delivery_time, 'j M Y g:i A'); ?></td>
 										<td><?PHP echo $arr->order_type; ?></td>
 										<td><?php echo $to_be_delivered; ?></td>
-										<td><?php echo $remaining_delivery_time; ?></td>
+										
+										<td class="countdown" data-time="<?= $arr->to_time ?>"><?php echo $remaining_delivery_time; ?></td>
+										
 										<td><?php echo $deliver_in; ?></td>
 										<td><?php echo $arr->orders_status_list_status; ?></td>
 										<td class="text-center"><a href="<?php echo SITE_URL.'online-order-details'; ?>?order_id=<?php echo($arr->orders_id);?>"><div class="wh-42 d-flex align-items-center justify-content-center rounded-circle bg-warning bg-opacity-10 text-warning" title = "View details" data-bs-toggle="tooltip">
@@ -204,6 +206,40 @@
 	<?PHP include_once("includes/footer.php"); ?>
 <!-- ######### FOOTER END ############### -->
 <script>
+function startCountdowns() {
+
+    document.querySelectorAll('.countdown').forEach(function(el) {
+
+        let endTime = new Date(el.dataset.time.replace(' ', 'T')).getTime();
+
+        function updateTimer() {
+            let now = new Date().getTime();
+            let diff = endTime - now;
+
+            if (diff <= 0) {
+                el.innerHTML = "<span style='color:red;font-weight:bold'>Expired</span>";
+                return;
+            }
+
+            let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+            let hrs  = Math.floor((diff / (1000 * 60 * 60)) % 24);
+            let mins = Math.floor((diff / (1000 * 60)) % 60);
+            let secs = Math.floor((diff / 1000) % 60);
+
+            let text = "";
+            if (days > 0) text += days + "d ";
+            if (hrs > 0) text += hrs + "h ";
+            if (mins > 0) text += mins + "m ";
+            text += secs + "s";
+
+            el.innerHTML = text;
+        }
+
+        updateTimer();
+        setInterval(updateTimer, 1000);
+    });
+}
+
 $(document).ready(function(){
 	if ($.fn.DataTable.isDataTable('#example2')) {
 		$('#example2').DataTable().destroy();
@@ -218,6 +254,10 @@ $(document).ready(function(){
             orderSequence: ['asc', 'desc'] // manual toggle only
         }
     ]
+	});
+	startCountdowns();
+	$('#example2').on('draw.dt', function () {
+		startCountdowns();
 	});
 });
 </script>
